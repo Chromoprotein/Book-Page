3b49b4b8df20dc909646da325b166a55c5fd1e6b
Chromoprotein <bridges.of.sighs@gmail.com>
2024-03-27 01:22:27 +0200

    frontend

---
+++ node-practice-frontend/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://node-practice-2-463982825488.herokuapp.com/post')
+    .then(res => {
+      console.log(res.data.data)
+      setPost(res.data.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {post.map(item => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+}
+
+export default App;
