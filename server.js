81af5097386af2025e29fa927584b7a884bed2f0
Chromoprotein <bridges.of.sighs@gmail.com>
2024-03-26 22:00:48 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const posts = [
+    {id: 1, name: "react", description: "javascript library"},
+    {id: 2, name: "node js", description: "javascrip runtime environment"}
+]
+
+app.get("/post", (req,res) => {
+    return res.status(200).json({
+        posts
+    })
+})
+
+app.listen(8000, () => {
+    console.log("server started on port 8000");
+});
