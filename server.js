0ee0a9b72d5f00d1753ed9218e3efa50e39c2884
Chromoprotein <bridges.of.sighs@gmail.com>
2024-03-27 02:58:57 +0200

    more building

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.use(express.json());
 
 require('./db')
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, 'node-practice-frontend/build')));
-
 app.get("/post", (req,res) => {
     Post
         .find()
@@ -27,15 +24,18 @@ app.get("/post", (req,res) => {
 
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'node-practice-frontend/build', 'index.html'));
-});
-
 app.get("/data", (req,res) => {
     return res.status(200).json({
         message:"success"
     })
 })
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, 'node-practice-frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'node-practice-frontend/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("server started on port 8000");
