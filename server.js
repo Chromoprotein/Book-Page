fb74b88bef997da432afb9bbfe8cb9e899b3c4ea
Chromoprotein <bridges.of.sighs@gmail.com>
2024-03-27 03:11:31 +0200

    cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const Post = require('./Post');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 require('./db')
